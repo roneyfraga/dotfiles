5695f773c30dbe7dfa3a75a1892327db99c97997
Roney Fraga Souza <roneyfraga@gmail.com>
2023-04-04 11:24:26 -0400

    old sh files deleted

--- bin/layout_keyboard_BR.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-setxkbmap -option caps:ctrl_modifier -option shift:both_capslock_cancel -option altwin:swap_lalt_lwin
--- bin/layout_keyboard_US.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-setxkbmap -layout us -variant intl -option caps:ctrl_modifier -option shift:both_capslock_cancel
