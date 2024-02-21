4b2d045cbd6b769427bc39fb37c9a66a81dc5f03
Roney Fraga Souza <roneyfraga@gmail.com>
2024-02-21 09:21:40 -0400

    doi2bibtex

--- bin/doi2bibtex.R
+++ bin/doi2bibtex.R
@@ -1,12 +1,22 @@
 #!/usr/bin/env Rscript
 
 # run
-# ./doi2bibtex.R doiCode
+# ./doi2bibtex.R DOIcode
 
 suppressMessages(library(rcrossref))
+suppressMessages(library(clipr))
+suppressMessages(library(stringr))
 
 # receber o comando do terminal
 args <- commandArgs(TRUE)
+args <- gsub('^.*.org\\/', '', args)
 
-cat(rcrossref::cr_cn(dois = args, format = "bibtex"))
+rcrossref::cr_cn(dois = args, format = "bibtex") -> bib 
+
+gsub('[', '', 'Vargas_Carpintero_2022, title={')
+
+clipr::write_clip(bib2, allow_non_interactive = TRUE)
+
+# args <- '10.3390/land11101748'
+
 
