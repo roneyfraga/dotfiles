5aeb7e3d67ac97e834581b40eed17c7193f6b1bf
Roney Fraga Souza <roneyfraga@gmail.com>
2024-10-29 14:45:29 -0400

    which-key adjust

--- nvim/init.lua
+++ nvim/init.lua
@@ -731,7 +731,7 @@ wk.add({
   -- main group
   { "<Space>/", "<cmd>lua require('fzf-lua').lgrep_curbuf()<CR>", desc = "search here" },
   { "<Space>b", "<cmd>lua require('fzf-lua').buffers()<CR>", desc = "find buffers" },
-  { "<Space>a", "<cmd>lua require('fzf-lua').lines()<CR>", desc = "all buffers" },
+  -- { "<Space>a", "<cmd>lua require('fzf-lua').lines()<CR>", desc = "all buffers" },
   { "<Space>w", "<cmd>w<CR>", desc = "write" },
   { "<Space>W", "<cmd>wa!<CR>", desc = "write all" },
   { "<Space>q", "<cmd>q!<CR>", desc = "quite" },
